efa83e1dd15984184d8bd0f4cb38fffd1f9b6ad6
Büşra Çekici <busracekici37@gmail.com>
2025-01-10 19:37:16 +0300

    fourth-commit

--- js/task-2.js
+++ js/task-2.js
@@ -1,12 +1,13 @@
 function getShippingMessage(country, price, deliveryFee) {
-    const totalPrice = price + deliveryFee
-    console.log(`Shipping to ${country} will cost ${totalPrice} credits`)
-    return `Shipping to ${country} will cost ${totalPrice} credits`
+    const totalPrice = price + deliveryFee;
+    
+    return `Shipping to ${country} will cost ${totalPrice} credits`;
     
 }
-getShippingMessage("Australia", 120, 50)
-getShippingMessage("Germany", 80, 20)
-getShippingMessage("Sweden", 100, 20)
+console.log(getShippingMessage("Australia", 120, 50));
+console.log(getShippingMessage("Germany", 80, 20)); 
+console.log(getShippingMessage("Sweden", 100, 20)); 
+
 
 
 // console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,13 @@
 function getElementWidth(content, padding, border) {
     const toplamGenişlik = (content) + (padding * 2) + (border * 2)
-    console.log(toplamGenişlik);
+
 
     return toplamGenişlik;
  }   
 
-getElementWidth(Number.parseFloat("50px"), Number.parseFloat("8px"), Number.parseFloat("4px"))
-getElementWidth(Number.parseFloat("60px"), Number.parseFloat("12px"), Number.parseFloat("8.5px"))
-getElementWidth(Number.parseFloat("200px"), Number.parseFloat("0px"), Number.parseFloat("0px"))
+console.log(getElementWidth(Number.parseFloat("50px"), Number.parseFloat("8px"), Number.parseFloat("4px")));
+console.log(getElementWidth(Number.parseFloat("60px"), Number.parseFloat("12px"), Number.parseFloat("8.5px")));
+console.log(getElementWidth(Number.parseFloat("200px"), Number.parseFloat("0px"), Number.parseFloat("0px")));
 
 
 
